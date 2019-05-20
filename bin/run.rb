0e47a5883d87fbc492152d459ad62ab6f87cd648
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-20 16:49:07 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,10 @@ require_relative "environment"
 Scraper.new(@db).scrape
 
 
+<<<<<<< HEAD
+=======
+@db.execute("ALTER TABLE pokemon ADD hp='60' INTEGER")
+>>>>>>> 7771a949ea1dd47ee01d6d399f6e64eb1a5a2c77
 all_pokemon = @db.execute("SELECT * FROM pokemon;")
 
 # test out your code here!
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,11 @@ class Pokemon
   attr_reader :id, :name, :type, :hp, :db
   @@all = []
 
+<<<<<<< HEAD
   def initialize (id:, name:, type:, hp: nil, db:)
+=======
+  def initialize (id, name, type, hp= 60, db)
+>>>>>>> 7771a949ea1dd47ee01d6d399f6e64eb1a5a2c77
     @id = id
     @name = name
     @type = type
@@ -10,6 +14,7 @@ class Pokemon
     @db = db
     @@all << self
   end
+#<<<<<<< HEAD
   
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
@@ -27,4 +32,6 @@ class Pokemon
     database_connection.execute("UPDATE pokemon SET hp = ? WHERE id = ?", hp, @id)
   end
     
+=======
+>>>>>>> 7771a949ea1dd47ee01d6d399f6e64eb1a5a2c77
 end
